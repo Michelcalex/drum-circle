a39db6a8c0c1c2636cc10cd58be927a7f31753a0
Michelcalex <Michelcalex+github@gmail.com>
2017-02-09 17:24:44 -0500

    Updated gulp file for static folders in src and build

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,39 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let browser = require('gulp-browser');
 
+const BUILD_URL = 'build/resources/main/static';
+const SRC_URL = 'src/main/resources/static';
+
 gulp.task('default', ['html', 'assets', 'css', 'js']);
 
 gulp.task('html', function () {
     gulp.src('components/*/*.html')
-        .pipe(gulp.dest('public/templates'));
+        .pipe(gulp.dest(BUILD_URL))
+        .pipe(gulp.dest(SRC_URL));
 
-    return gulp.src('index.html')
-        .pipe(gulp.dest('public/'));
+    return gulp.src('*.html')
+        .pipe(gulp.dest(BUILD_URL))
+        .pipe(gulp.dest(SRC_URL));
 });
 
 gulp.task('assets', function() {
     return gulp.src(['assets/*.ico', 'assets/*.jpg', 'assets/*.png'])
-        .pipe(gulp.dest('public/assets'));
+        .pipe(gulp.dest(BUILD_URL))
+        .pipe(gulp.dest(SRC_URL));
 });
 
 gulp.task('css', function () {
     return gulp.src('styles/style.scss')
         .pipe(sass()) 
-        .pipe(gulp.dest('public/'));
+        .pipe(gulp.dest(BUILD_URL))
+        .pipe(gulp.dest(SRC_URL));
 });
 
 gulp.task('js', function () {
     return gulp.src('js/app.js')
-        .pipe(browser.browserify())
-        .pipe(gulp.dest('public/'));
+        //note you took browersify out
+        .pipe(gulp.dest(BUILD_URL))
+        .pipe(gulp.dest(SRC_URL));
 });
 
 gulp.task('watch', ['default'], function () {
