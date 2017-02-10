c311a5e7012bf816492df1cfef9d81b09351b05b
Michelcalex <Michelcalex+github@gmail.com>
2017-02-10 11:11:54 -0500

    defined run function to switch states based on login

--- js/app.js
+++ js/app.js
@@ -39,4 +39,21 @@ app.config(function ($stateProvider) {
         url: '/login',
         component: 'welcome',
     });
+}).run(function ($location, $state) {
+    // There is probably a cleaner way to do this; specifically it feels kinda
+    // gross because we have to put file names in here (which could change).
+    // Should work fine for you guys for now, but this will break if you
+    // rename your html files.
+
+    // Once routes are setup, move to a different default route based on 
+    // what file we've loaded.
+
+
+    // If the url includes 'index.html', redirect to the main app state.
+    if ($location.absUrl().includes('index.html')) {
+
+    } else {
+        $state.go('welcome');
+    }
+
 });
