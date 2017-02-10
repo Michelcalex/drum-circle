e82060bdd9fb96f3f85756907dbbc98dda7ccb3f
Benjamin Burr <bburrva@gmail.com>
2017-02-10 11:41:53 -0500

    began work on sounds class

--- src/main/java/com/burr/benjamin/entities/Sound.java
+++ src/main/java/com/burr/benjamin/entities/Sound.java
@@ -1,7 +1,25 @@
 package com.burr.benjamin.entities;
 
+import javax.persistence.*;
+
 /**
  * Created by Ben on 2/9/17.
  */
+
+@Entity
+@Table(name = "sounds")
 public class Sound {
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String category;
+
+    @Column(nullable = false)
+    
 }
