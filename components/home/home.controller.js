749ebb2ef318d6b64b8b7e7900d45c7dd920502a
Michelcalex <Michelcalex+github@gmail.com>
2017-02-15 16:54:47 -0500

    Added capitalization filter and minor changes to Welcome <username> functionality

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -5,20 +5,6 @@ module.exports = {
        HomeService.getUser().then(function (name) {
         $scope.name = name;
        });
-       
-    //    .then(function(response){
-    //        let user = response.data;
-    //        console.log(user);
-    //    })
-    //    HomeService.getUser().then(function(response) {
-    //        let user = response.data;
-    //        if (user !== null) {
-    //            console.log('cool dude')
-    //        } else {
-    //            console.log("this is not working");
-    //        console.log(user);
-    //    }
-    //  });
     },
 };
 
--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ const controllers = [
     require('../components/browse/browse.controller'),
     require('../components/browse/browse.tabs.controller'),
     require('../components/home/home.controller'),
+    require('../components/header/header.controller'),
 ];
 
 for (let i = 0; i < controllers.length; i++) {
@@ -38,6 +39,17 @@ const services = [
 for (let i = 0; i < services.length; i++) {
     app.factory(services[i].name, services[i].func);
 }
+
+
+
+
+//Filters -------------------------------------------------------
+app.filter('capitalize', function() {
+    return function(input) {
+        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
+});
+
 
 
 //States ---------------------------------------------------------
@@ -73,23 +85,3 @@ app.config(function ($stateProvider) {
         component: 'kit',
     });
 });
-
-// window.addEventListener('load', function() {
-//     getUser().then(function(response) {
-//         let answer = response;
-//         console.log(answer);
-//     })
-// })
-
-
-// .then(function(response){
-    //        let user = response.data;
-    //        console.log(user);
-    //    })
-    //    HomeService.getUser().then(function(response) {
-    //        let user = response.data;
-    //        if (user !== null) {
-    //            console.log('cool dude')
-    //        } else {
-    //            console.log("this is not working");
-    //        console.log(user);
