befae7b6ff9dc0f2881baef650af8787f08e8049
Leo <leowolf749@gmail.com>
2017-02-22 12:53:16 -0500

    Mapped the keys to their corresponding pads. Accessed the state to limit the keys to only play the sounds when the user is on the kit page. Cleaned up some old comments and console logs.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -109,18 +109,68 @@ module.exports = {
 },{}],7:[function(require,module,exports){
 module.exports = {
     name: 'KitController',
-    func: function($scope, KitService, dragulaService) {
+    func: function($scope, KitService, dragulaService, $document, $state) {
         // playList[0] is the index of the sound that should be played when 0 is clicked
         const playList = [];
 
+        $document.bind('keypress', function (event) {
+            if ($state.current.url !== '/kit') {
+                return;
+            }
+            if (event.key === '1') {
+                KitService.playKitSounds(playList[0]);
+            }
+            if (event.key === '2') {
+                KitService.playKitSounds(playList[1]);
+            }
+            if (event.key === '3') {
+                KitService.playKitSounds(playList[2]);
+            }
+            if (event.key === '4') {
+                KitService.playKitSounds(playList[3]);
+            }
+            if (event.key === 'q') {
+                KitService.playKitSounds(playList[4]);
+            }
+            if (event.key === 'w') {
+                KitService.playKitSounds(playList[5]);
+            }
+            if (event.key === 'e') {
+                KitService.playKitSounds(playList[6]);
+            }
+            if (event.key === 'r') {
+                KitService.playKitSounds(playList[7]);
+            }
+            if (event.key === 'a') {
+                KitService.playKitSounds(playList[8]);
+            }
+            if (event.key === 's') {
+                KitService.playKitSounds(playList[9]);
+            }
+            if (event.key === 'd') {
+                KitService.playKitSounds(playList[10]);
+            }
+            if (event.key === 'f') {
+                KitService.playKitSounds(playList[11]);
+            }
+            if (event.key === 'z') {
+                KitService.playKitSounds(playList[12]);
+            }
+            if (event.key === 'x') {
+                KitService.playKitSounds(playList[13]);
+            }
+            if (event.key === 'c') {
+                KitService.playKitSounds(playList[14]);
+            }
+            if (event.key === 'v') {
+                KitService.playKitSounds(playList[15]);
+            }                          
+        });
+
         $scope.play = function (slot, event) {
             if (playList[slot] !== undefined) {
-                // todo: play wad
                 KitService.playKitSounds(playList[slot]);
-            }
-            // if (playList[slot] !== undefined && event.keyCode == 81) {
-            //     KitService.playKitSounds(playList[slot]);
-             else {
+            } else {
                 console.log(`Slot ${slot} doesnt have a sound assigned.`);
             }
         };
@@ -427,7 +477,7 @@ module.exports = {
             },
 
             playKitSounds(index) {
-                console.log(wads[index]);
+                // console.log(wads[index]);
                 wads[index].play();
             },
 
@@ -439,7 +489,7 @@ module.exports = {
                 favoriteList.push(sound);
                 addWad(sound);
 
-                console.log(favoriteList);
+                // console.log(favoriteList);
             },
 
             markUnFavorite(sound) {
