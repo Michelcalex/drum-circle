7b9c01ae406ae6ad9dfea9f8b47290155d85ab2d
Benjamin Burr <bburrva@gmail.com>
2017-02-22 19:11:25 -0500

    changing sign up success message as per front end request. removed login success message

--- src/main/java/com/burr/benjamin/controllers/HomeController.java
+++ src/main/java/com/burr/benjamin/controllers/HomeController.java
@@ -64,7 +64,6 @@ public class HomeController {
         User user = users.findByUsername(username);
         if (user != null && PasswordStorage.verifyPassword(password, user.getPassword())) {
             session.setAttribute("user", user.getId());
-            session.setAttribute("success", "Login Successful.");
         } else {
             session.setAttribute("error", "You entered an invalid username or password.");
         }
@@ -85,7 +84,7 @@ public class HomeController {
             try {
                 user = new User(username, PasswordStorage.createHash(password));
                 users.save(user);
-                session.setAttribute("success", "Sign up successful. Please Login.");
+                session.setAttribute("success", "Awesome sauce! Now please Login.");
             } catch (PasswordStorage.CannotPerformOperationException e) {
                 e.printStackTrace();
             }
